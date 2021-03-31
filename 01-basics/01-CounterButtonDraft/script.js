8c6fca98d761819e0e76220590f0398a66d369f1
sqadirova <sidiqa@inbox.ru>
2021-03-31 17:06:42 +0400

    add solution for CounterButtonDraft

--- 01-basics/01-CounterButtonDraft/script.js
+++ 01-basics/01-CounterButtonDraft/script.js
@@ -1,3 +1,18 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
-// new Vue();
+new Vue({
+  el:'#app',
+
+  data(){
+    return {
+      btnValue:0,
+    };
+  },
+
+  methods:{
+    counterBtn(){
+      this.btnValue+=1;
+    },
+  },
+
+});
