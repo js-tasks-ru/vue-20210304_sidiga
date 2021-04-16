a29aabf55e884ec09f44120d1d9fe5f853251656
sqadirova <sidiqa@inbox.ru>
2021-04-16 15:08:22 +0400

    add solution for MeetupTitle

--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -1,3 +1,32 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
-// new Vue();
+new Vue({
+  el:'#app',
+
+  data(){
+    return{
+      meetupId:"",
+      meetup:"",
+
+    }
+  },
+  watch:{
+    meetupId(){
+      this.handleMeetupIdChange();
+    }
+  },
+
+
+  methods:{
+    handleMeetupIdChange(){
+      fetch(`https://course-vue.javascript.ru/api/meetups/${this.meetupId}`)
+        .then((res)=>res.json())
+        .then(meetup=>{
+          this.meetup=meetup
+        })
+    },
+
+  }
+
+
+});
